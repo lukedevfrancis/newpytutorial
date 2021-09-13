ade67ee5352c5b40001563a63c3dd66cae108cfc
lukedevfrancis <lukeyyfrancis@gmail.com>
2021-09-13 18:15:55 -0400

    worked with data and values

---
+++ datavals.py
@@ -0,0 +1,7 @@
+character_name = "Max"
+character_age = "38"
+
+print("There once was a man named " + character_name + ", ")
+print("He was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but he didn't like being " + character_age + ".")
