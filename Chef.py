8cb476b8309dad7616b3a5842b8c289df0969803
lukedevfrancis <lukeyyfrancis@gmail.com>
2022-01-19 17:32:19 -0500

    added method

--- Chef.py
+++ Chef.py
@@ -7,6 +7,8 @@ class Chef:
         def make_salad(self):
             print("The chef makes a salad")
 
+        def make_pasta(self):
+            print("The chef makes pasta")
+
         def make_special_dish(self):
             print("The chef makes bbq ribs")
-                                            
