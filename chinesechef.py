843e2223c1158e3f740bfa9980c55fdaf471dbac
lukedevfrancis <lukeyyfrancis@gmail.com>
2022-01-23 21:00:06 -0500

    added function

---
+++ chinesechef.py
@@ -0,0 +1,12 @@
+from Chef import Chef
+
+class ChineseChef(Chef):
+
+    def make_special_dish(self):
+        print("The chef makes orange chicken")
+
+    def make_fried_rice(self):
+        print("The chef makes fried rice")
+
+    def make_eggroll(self):
+        print("The chef makes eggroll")
